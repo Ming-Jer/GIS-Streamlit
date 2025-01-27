79cb85a1229ad7a2750b74afc82b6d2cb385a09a
TGI-ed <ed@gideons.tw>
2025-01-27 18:23:29 +0800

    Update main program

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,10 @@ def load_cities(city_file):
     return df
 
 st.title("GIS Demo App")
+st.header("A simple demonstration that displays world cities using Streamlit's basic map function.")
+st.markdown('''
+World Cities Database is a comprehensive, up-to-date collection of global cities and towns maintained by Simple Maps (https://simplemaps.com/data/world-cities). 
+It is built using trusted sources including the NGIA, US Geological Survey, US Census Bureau, and NASA.            
+            ''')
 cities=load_cities("data/worldcities.csv")
 st.map(cities, latitude="lat", longitude="lng")
